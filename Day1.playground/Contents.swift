5ed9f1a16b58bdbcacc80b71c9c519dcf9230f3c
ayemereodia2 <odiadavid2@gmail.com>
2023-01-05 01:49:59 +0100

    Container with most water brute force solution

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Container With Most Water Problem
+// Given an Int array input of height of length N. There are N vertical lines drawn such that the
+// two endpoints of ith line are (i,0) (i, height[i])
+// find the two lines that together with the X-axis form a container, such that the container contains most water.
+// return the maximum amount of water a container can store.
+
+// Hint this problem has nothing to do with Water.
+// Area = min(h1,h2) * width
+// where width is 1 unit from each height
+
+// we take min of two heights since any water over the minimum height will spill over and therefore
+// cannot be held by the container
+
+//BRUTE FORCE SOLUTION 1
+
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var maxi = 0
+        for index in 0..<height.count {
+            let start = index + 1
+            
+            for innerIndex in start..<height.count {
+                var distance = innerIndex - index
+                let result = min(height[index], height[innerIndex])  * distance
+                maxi = max(result, maxi)
+            }
+        }
+        
+        return maxi
+    }
+}
+// Big O of N2 // because loop 1 is O(N * (N - 1)) = (N2 - N)
+// ignoring the least dorminant term Big O is N2
+
