5ae0213c794043fe7f0f2b6623d485cf5ca0b76b
ayemereodia2 <odiadavid2@gmail.com>
2024-04-22 11:51:05 -0300

    added an efficient way of climbStairs

--- Easy.playground/Contents.swift
+++ Easy.playground/Contents.swift
@@ -1208,4 +1208,28 @@ class ClimbStairsSolution {
            return climbStairs(n - 1) + climbStairs(n - 2)
         }
     }
+  
+  /*
+   Using a bottom-up iterative approach approach with a better runtime, no need to maintain a high call stack
+   */
+  func climbStairsEff(_ n: Int) -> Int {
+      if n == 1 {
+          return 1
+      }
+      if n == 2 {
+          return 2
+      }
+
+      var dp = [Int](repeating: 0, count: n + 1)
+      dp[1] = 1
+      dp[2] = 2
+      
+      for i in 3...n {
+          dp[i] = dp[i - 1] + dp[i - 2]
+      }
+      
+      return dp[n]
+  }
 }
+
+
